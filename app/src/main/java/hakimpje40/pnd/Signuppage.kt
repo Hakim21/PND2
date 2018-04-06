7c1b6ffc762bff14ec126e8ba08a81002d47fb70
Hakim21 <up712221@myport.ac.uk>
2018-04-06 16:13:06 +0100

    05/04/2018

--- app/src/main/java/hakimpje40/pnd/Signuppage.kt
+++ app/src/main/java/hakimpje40/pnd/Signuppage.kt
@@ -1,25 +1,12 @@
 package hakimpje40.pnd
 
 import android.content.Intent
-import android.nfc.Tag
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Message
-import android.provider.ContactsContract
-import android.text.LoginFilter
-import android.util.Log
-import android.view.View
-import android.widget.Toast
-import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.AuthCredential
-import com.google.firebase.auth.EmailAuthProvider
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthUserCollisionException
-import kotlinx.android.synthetic.main.homepage.*
+import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.signuppage.*
-import java.net.PasswordAuthentication
 
 class Signuppage : AppCompatActivity() {
+
 
 
 
@@ -27,12 +14,14 @@ class Signuppage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.signuppage)
 
-        // after sign up go to login page !
-        signup.setOnClickListener{
-            val i = Intent(this,Loginpage::class.java)
-            startActivity(i)
+
+
+            // after sign up go to login page !
+            signup.setOnClickListener{
+                val i = Intent(this,Loginpage::class.java)
+                startActivity(i)
+            }
+
+
         }
-
-
     }
-}
